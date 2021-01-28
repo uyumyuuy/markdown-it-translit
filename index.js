12ad9649beab5dc4cee2f95fefffada520b5fc61
uyum <54132088+uyumyuuy@users.noreply.github.com>
2021-01-28 20:31:40 +0900

    出力HTMLを変更

--- index.js
+++ index.js
@@ -1,180 +1,260 @@
 "use strict";
 
-function to_sub(x) {
-  if (x === "x") {
-    return "ₓ"
-  } else {
-    var n = Number(x)
-    return String.fromCharCode(0x2080+n)
-  }
+function convert(text) {
+  text = text.replace(/c/g, "š");
+  text = text.replace(/C/g, "Š");
+  text = text.replace(/sz/g, "š");
+  text = text.replace(/SZ/g, "Š");
+  text = text.replace(/s,/g, "ṣ");
+  text = text.replace(/S,/g, "Ṣ");
+  text = text.replace(/t,/g, "ṭ");
+  text = text.replace(/T,/g, "Ṭ");
+  text = text.replace(/t_/g, "ṯ");
+  text = text.replace(/T_/g, "Ṯ");
+  text = text.replace(/j/g, "ŋ");
+  text = text.replace(/J/g, "Ŋ");
+  text = text.replace(/g~/g, "g̃");
+  text = text.replace(/G~/g, "G̃");
+  text = text.replace(/h,/g, "ḥ");
+  text = text.replace(/h/g, "ḫ");
+  text = text.replace(/H/g, "Ḫ");
+  text = text.replace(/a\^/g, "â");
+  text = text.replace(/i\^/g, "î");
+  text = text.replace(/u\^/g, "û");
+  text = text.replace(/e\^/g, "ê");
+  text = text.replace(/o\^/g, "ô");
+  text = text.replace(/A\^/g, "Â");
+  text = text.replace(/I\^/g, "Î");
+  text = text.replace(/U\^/g, "Û");
+  text = text.replace(/E\^/g, "Ê");
+  text = text.replace(/O\^/g, "Ô");
+  text = text.replace(/a~/g, "ā");
+  text = text.replace(/i~/g, "ī");
+  text = text.replace(/u~/g, "ū");
+  text = text.replace(/e~/g, "ē");
+  text = text.replace(/o~/g, "ō");
+  text = text.replace(/A~/g, "Ā");
+  text = text.replace(/I~/g, "Ī");
+  text = text.replace(/U~/g, "Ū");
+  text = text.replace(/E~/g, "Ē");
+  text = text.replace(/O~/g, "Ō");
+  text = text.replace(/'/g, "ʿ"); //ayin
+  text = text.replace(/`/g, "ʾ"); //aleph
 
+  return text;
 }
 
-function transliterate_index(content, state) {
-  var m = content.match(/^([^0-9x]+)([0-9x])$/);
-  if (m) {
-    content = m[1] + m[2].split().map(to_sub).join("")
-  }
-  if (content === content.toUpperCase()) {
-    state.push("em_open", "em", 1);
-    var token = state.push("text", "", 0);
-    token.content = content;
-    state.push("em_close", "em", -1);
-  } else {
-    var token = state.push("text", "", 0);
-    token.content = content;
-  }
-}
+function create_rule(type, accentIndex, convertTypography, useSubSupUnicode) {
+  const first_marker = "=".charCodeAt(0);
+  const second_maker_sumerian = "=".charCodeAt(0);
+  const second_marker_akkadian = "*".charCodeAt(0);
 
-function transliterate(content, state) {
-  var splitter = /([ ().-])/
-  var tokens = content.split(/({[^}]+)}|([ ().-])/);
-  tokens.forEach((text) => {
-    if (!text) return;
-    if (text == "") return;
-    if (splitter.test(text)) {
-      var token = state.push("text", "", 0)
-      token.content = text
+  let akkadian;
+  if (type === "akkadian") {
+    akkadian = true;
+  } else {
+    akkadian = false;
+  }
+
+  function to_sub(x) {
+    if (x === "x") {
+      return "ₓ";
     } else {
+      var n = Number(x);
+      return String.fromCharCode(0x2080 + n);
+    }
+  }
+
+  function transliterate_index(content, state) {
+    if (convertTypography) {
+      content = convert(content);
+    }
+    var m = content.match(/^([^0-9x]+)([0-9]*[0-9x])$/);
+    if (m) {
+      const capital = akkadian && m[1] === m[1].toUpperCase();
+      const number = Number(m[2]);
+      if (capital) {
+        state.push("em_open", "em", 1);
+      }
+      if (accentIndex && (number === 2 || number === 3)) {
+        const accent = function (c) {
+          return {
+            a: ["a", "á", "à"],
+            i: ["i", "í", "ì"],
+            u: ["u", "ú", "ù"],
+            e: ["e", "é", "è"],
+            A: ["A", "Á", "À"],
+            I: ["I", "Í", "Ì"],
+            U: ["U", "Ú", "Ù"],
+            E: ["E", "É", "È"],
+          }[c][number - 1];
+        };
+        var token = state.push("text", "", 0);
+        token.content = m[1].replace(/[aiueAIUE]/, accent);
+      } else if (useSubSupUnicode) {
+        var token = state.push("text", "", 0);
+        token.content = m[1] + m[2].split("").map(to_sub).join("");
+      } else {
+        var token = state.push("text", "", 0);
+        token.content = m[1];
+        state.push("sub_open", "sub", 1);
+        var token = state.push("text", "", 0);
+        token.content = m[2];
+        state.push("sub_close", "sub", -1);
+      }
+      if (capital) {
+        state.push("em_close", "em", -1);
+      }
+    } else {
+      if (
+        akkadian &&
+        content === content.toUpperCase() &&
+        !content.match(/[ .\-()]/)
+      ) {
+        state.push("em_open", "em", 1);
+        var token = state.push("text", "", 0);
+        token.content = content;
+        state.push("em_close", "em", -1);
+      } else {
+        var token = state.push("text", "", 0);
+        token.content = content;
+      }
+    }
+  }
+
+  function transliterate(content, state) {
+    var tokens = content.split(/({[^}]+)}|([ .\-()])/);
+    tokens.forEach((text) => {
+      if (!text) return;
+      if (text == "") return;
       var ch = text.charCodeAt(0);
       if (ch === 0x7b /* { */) {
+        if (useSubSupUnicode) {
+          switch (text.slice(1)) {
+            case "d":
+              state.push("text", "", 0).content = "ᵈ";
+              return;
+            case "f":
+              state.push("text", "", 0).content = "ᶠ";
+              return;
+            case "m":
+              state.push("text", "", 0).content = "ᵐ";
+              return;
+            case "ki":
+              state.push("text", "", 0).content = "ᵏⁱ";
+              return;
+          }
+        }
         state.push("sup_open", "sup", 1);
         transliterate_index(text.slice(1), state);
         state.push("sup_close", "sup", -1);
       } else {
         transliterate_index(text, state);
-      }  
-    }
-  });
-}
-
-function tokenize(state, silent) {
-  var found,
-    content,
-    token,
-    max = state.posMax,
-    start = state.pos;
-
-  const equal = "=".charCodeAt(0);
-
-  if (state.src.charCodeAt(start) !== equal /* = */) {
-    return false;
-  }
-  if (silent) {
-    return false;
-  } // don't run any pairs in validation mode
-  if (start + 4 >= max) {
-    return false;
-  }
-
-  state.pos++;
-  if (state.src.charCodeAt(state.pos) !== equal /* = */) {
-    state.pos = start;
-    return false;
-  }
-
-  state.pos++;
-  while (state.pos < max - 1) {
-    if (state.src.charCodeAt(state.pos) === equal /* = */) {
-      state.pos++;
-      if (state.src.charCodeAt(state.pos) === equal /* = */) {
-        found = true;
-        break;
       }
+    });
+  }
+
+  return function tokenize(state, silent) {
+    var found,
+      content,
+      token,
+      max = state.posMax,
+      start = state.pos,
+      second_marker;
+
+    if (akkadian) {
+      second_marker = second_marker_akkadian;
+    } else {
+      second_marker = second_maker_sumerian;
     }
-    state.md.inline.skipToken(state);
-  }
 
-  if (!found || start + 2 === state.pos) {
-    state.pos = start;
-    return false;
-  }
+    if (state.src.charCodeAt(start) !== first_marker) {
+      return false;
+    }
+    if (silent) {
+      return false;
+    } // don't run any pairs in validation mode
+    if (start + 4 >= max) {
+      return false;
+    }
 
-  content = state.src.slice(start + 2, state.pos - 1);
+    state.pos++;
+    if (state.src.charCodeAt(state.pos) !== second_marker) {
+      state.pos = start;
+      return false;
+    }
 
-  state.posMax = state.pos;
-  state.pos = start + 2;
-
-  token = state.push("translit_open", "span", 1);
-  token.markup = "==";
-  token.attrJoin("class", "sumero-transliteral");
-
-  transliterate(content, state);
-
-  token = state.push("translit_close", "span", -1);
-  token.markup = "==";
-
-  state.pos = state.posMax + 1;
-  state.posMax = max;
-  return true;
-}
-
-function tokenize_akkadian(state, silent) {
-  var found,
-    content,
-    token,
-    max = state.posMax,
-    start = state.pos;
-  const equal = "=".charCodeAt(0);
-  const asterisk = "*".charCodeAt(0);
-
-  if (state.src.charCodeAt(start) !== equal) {
-    return false;
-  }
-  if (silent) {
-    return false;
-  } // don't run any pairs in validation mode
-  if (start + 4 >= max) {
-    return false;
-  }
-
-  state.pos++;
-  if (state.src.charCodeAt(state.pos) !== asterisk) {
-    state.pos = start;
-    return false;
-  }
-
-  state.pos++;
-  while (state.pos < max - 1) {
-    if (state.src.charCodeAt(state.pos) === asterisk) {
-      state.pos++;
-      if (state.src.charCodeAt(state.pos) === equal) {
-        found = true;
-        break;
+    state.pos++;
+    while (state.pos < max - 1) {
+      if (state.src.charCodeAt(state.pos) === second_marker) {
+        state.pos++;
+        if (state.src.charCodeAt(state.pos) === first_marker) {
+          found = true;
+          break;
+        }
       }
+      state.md.inline.skipToken(state);
     }
-    state.md.inline.skipToken(state);
-  }
 
-  if (!found || start + 2 === state.pos) {
-    state.pos = start;
-    return false;
-  }
+    if (!found || start + 2 === state.pos) {
+      state.pos = start;
+      return false;
+    }
 
-  content = state.src.slice(start + 2, state.pos - 1);
+    content = state.src.slice(start + 2, state.pos - 1);
 
-  state.posMax = state.pos;
-  state.pos = start + 2;
+    state.posMax = state.pos;
+    state.pos = start + 2;
 
-  token = state.push("translit_open", "span", 1);
-  token.markup = "=*";
-  token.attrJoin("class", "akkado-transliteral");
+    token = state.push("translit_open", "span", 1);
+    if (akkadian) {
+      token.markup = "=*";
+      token.attrJoin("class", "akkado-transliteral");
+    } else {
+      token.markup = "==";
+      token.attrJoin("class", "sumero-transliteral");
+    }
 
-  transliterate(content, state);
+    transliterate(content, state);
 
-  token = state.push("translit_close", "span", -1);
-  token.markup = "*=";
+    token = state.push("translit_close", "span", -1);
+    if (akkadian) {
+      token.markup = "*=";
+    } else {
+      token.markup = "==";
+    }
 
-  state.pos = state.posMax + 1;
-  state.posMax = max;
-  return true;
+    state.pos = state.posMax + 1;
+    state.posMax = max;
+    return true;
+  };
 }
 
-module.exports = function ins_plugin(md) {
-  md.inline.ruler.before("emphasis", "assyriology-translit1", tokenize);
+module.exports = function ins_plugin(md, options) {
+  var defaults = {
+    accentIndex: false,
+    useSubSupUnicode: false,
+    convertTypography: false,
+  };
+  var opts = md.utils.assign({}, defaults, options || {});
+  md.inline.ruler.before(
+    "emphasis",
+    "assyriology-translit1",
+    create_rule(
+      "sumerian",
+      opts.accentIndex,
+      opts.convertTypography,
+      opts.useSubSupUnicode
+    )
+  );
   md.inline.ruler.before(
     "emphasis",
     "assyriology-translit2",
-    tokenize_akkadian
+    create_rule(
+      "akkadian",
+      opts.accentIndex,
+      opts.convertTypography,
+      opts.useSubSupUnicode
+    )
   );
 };
--- test/test.js
+++ test/test.js
@@ -10,3 +10,30 @@ describe("markdown-it-assyriology-translit", function () {
 
   generate(path.join(__dirname, "fixtures/translit.txt"), md);
 });
+
+describe("markdown-it-assyriology-translit", function () {
+  var md = require("markdown-it")().use(require("../"), {
+    accentIndex: true,
+    useSubSupUnicode: false,
+    convertTypography: false,
+  });
+  generate(path.join(__dirname, "fixtures/accent_index.txt"), md);
+});
+
+describe("markdown-it-assyriology-translit", function () {
+  var md = require("markdown-it")().use(require("../"), {
+    accentIndex: false,
+    useSubSupUnicode: true,
+    convertTypography: false,
+  });
+  generate(path.join(__dirname, "fixtures/subsup_unicode.txt"), md);
+});
+
+describe("markdown-it-assyriology-translit", function () {
+  var md = require("markdown-it")().use(require("../"), {
+    accentIndex: false,
+    useSubSupUnicode: false,
+    convertTypography: true,
+  });
+  generate(path.join(__dirname, "fixtures/convert_typography.txt"), md);
+});
